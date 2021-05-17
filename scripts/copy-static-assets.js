3f3853261cc17e47519257e4ebd48fde81505180
Nick O'Leary <nick.oleary@gmail.com>
2021-05-17 21:22:37 +0100

    Add i18n catalog

--- scripts/copy-static-assets.js
+++ scripts/copy-static-assets.js
@@ -1,19 +1,25 @@
 const minify = require("html-minifier").minify;
-const fs = require("fs").promises;
+const fs = require("fs-extra");
 const path = require("path");
 
 const projectRoot = path.join(__dirname,"..")
 const dist = path.join(projectRoot,"dist");
 const src = path.join(projectRoot,"src");
 const assets = [
-    "flow-debugger.html"
+    "flow-debugger.html",
+    "locales"
 ];
 
 (async function() {
     await fs.mkdir(dist,{recursive: true});
     for (let i=0; i<assets.length; i++) {
-        const content = await fs.readFile(path.join(src,assets[i]),"utf-8")
-        await fs.writeFile(path.join(dist,assets[i]), minify(content, {minifyCSS: true, minifyJS: true}))
+        if (/\.html/.test(assets[i])) {
+            const content = await fs.readFile(path.join(src,assets[i]),"utf-8")
+            await fs.writeFile(path.join(dist,assets[i]), minify(content, {minifyCSS: true, minifyJS: true}))
+        } else {
+            await fs.mkdir(path.join(dist,assets[i]), {recursive: true});
+            await fs.copy(path.join(src,assets[i]),path.join(dist,assets[i]))
+        }
     }
 })().catch(err => {
     console.error(err);
